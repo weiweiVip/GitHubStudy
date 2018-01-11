e0c395bc66bb82667821f5b909c2c76cd9d7be2d
luhongwei <845826379@qq.com>
2018-01-11 15:56:43 +0800

    增加一个测试模块

---
+++ mylibrary/src/main/java/com/capinfo/mylibrary/Util.java
@@ -0,0 +1,13 @@
+package com.capinfo.mylibrary;
+
+/**
+ * Created by luhongwei on 2018/1/11.
+ */
+
+public class Util {
+
+    public static String getHello(){
+        return "hello world!";
+    }
+
+}
